6b4e0304fc3cac35209c2ac6918d6ddbf73abb5d
Naga Pavan Kumar Kola <71481642+kolanagapavankumar@users.noreply.github.com>
2025-06-06 11:33:51 +0530

    Project 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Example: form submit event
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Message sent.");
+});
